f391e6b6288714ddc09c06de126d0fbc96312a3c
Sameid <sameid.usmani@gmail.com>
2015-05-04 17:11:33 -0400

    small

--- js/shape.js
+++ js/shape.js
@@ -1,3 +1,7 @@
+var randomInt = function(min, max){
+	return Math.floor(min + Math.random()*(max-min));
+}
+
 var Shape = function (){
 
 	this.width = 100;
@@ -6,14 +10,20 @@ var Shape = function (){
 	this.x = 0;
 	this.y = -100;
 
-	this.push = Math.floor(0 + Math.random()*(game.width-this.width-0));
+	var min = game.width* 0.2;
+	var max = game.width* 0.8;
+
+	this.push = randomInt(min,max);
 
 
 	this.colors = ["#80FF72", "#FFF07C", "#7EE8FA"];
 	this.color = this.colors[Math.floor(0 + Math.random()*(3-0))]
 
+	 this.horizontalStretch = randomInt(100, 300);
+
 
 }
+
 
 Shape.prototype.draw = function(ctx){
 
@@ -22,16 +32,18 @@ Shape.prototype.draw = function(ctx){
 	// ctx.rotate(20*Math.PI/180);
 
     ctx.shadowColor = 'black';
-    ctx.shadowBlur = 5;
-    ctx.shadowOffsetX = 0;
-    ctx.shadowOffsetY = 0;
+    // ctx.shadowBlur = 5;
+    // ctx.shadowOffsetX = 0;
+    // ctx.shadowOffsetY = 0;
 	ctx.fillRect(this.x,this.y,this.width,this.height);
 	
 }
 
 Shape.prototype.move = function(){
 	this.y += this.speed;
-	this.x = (Math.sin(this.y/100) *100) + this.push;
+
+	// var verticleStretch = Math.floor(0 + Math.random())
+	this.x = (Math.sin(this.y/this.horizontalStretch) * 100) + this.push;
 }
 
 Shape.prototype.isOutside = function(){
